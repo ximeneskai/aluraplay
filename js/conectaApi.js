34505158be3589d3c2e5aa7a559c5134f856d8ae
Kai <kaiximenes@outlook.com>
2024-10-16 19:20:46 -0300

    Aula finalizada, teste de busca e inserção: OK

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -19,6 +19,10 @@ async function criaVideo(titulo, descricao, url, imagem) {
         })
     });
 
+    if (!conexao.ok) {
+        throw new Error("Não foi possível enviar o vídeo.")
+    }
+
     const conexaoConvertida = conexao.json();
 
     return conexaoConvertida;
